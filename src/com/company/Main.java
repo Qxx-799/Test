00ea66d8a4c207b98b1b797f6d8d0c4888dea6ab
Qxx-799 <qixx_799@163.com>
2020-04-26 14:54:51 +0800

    第四次提交

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,6 +6,7 @@ public class Main  {
     public static void main(String[] args) {
         System.out.println("hello,longdongqiang");
         System.out.println("hello,haodongqiang");
+        System.out.println("hello,qidongqiang");
     }
 }
 

5c7efc222ad01bee8e6d46dcf13a0875b71d6d65
Qxx-799 <60804123+Qxx-799@users.noreply.github.com>
2020-04-26 14:52:25 +0800

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,7 @@ public class Main  {
 
     public static void main(String[] args) {
         System.out.println("hello,longdongqiang");
+        System.out.println("hello,haodongqiang");
     }
 }
 
